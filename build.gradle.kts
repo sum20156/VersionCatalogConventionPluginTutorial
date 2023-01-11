c293a714f1b4f23f372db8a5b1310cdc2ebfd90b
suman <suman.arun@anywhere.co>
2023-01-11 08:09:46 +0530

    root build gradle plugin refactored

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,7 @@ buildscript {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("com.android.tools.build:gradle-api:7.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
+
 }
 
 /*plugins{
@@ -24,4 +18,11 @@ allprojects {
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
 }
